0ba1b8d0a12b58b2e80f6a0802dd47908a363fba
Lola Onanuga <slonanuga@gmail.com>
2018-09-25 17:13:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+ array.map! {|name| badge_maker(name)}
+end
+
+def assign_rooms(speakers)
+  speakers.each_with_index.map do |name, idx|
+   
+  puts  "Hello, #{name} ! You'll be assigned to room #{idx+1}!"
+end
+end
+
+def printer(attendees)
+  
+  batch_badge_creator(attendees).each {|x|
+ puts x}
+ 
+ assign_rooms(attendees).each {|n| 
+puts n}
+end
+
+assign_rooms(["stac", "dan", "rub"])
